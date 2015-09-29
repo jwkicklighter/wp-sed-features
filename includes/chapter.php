83c589e2d768d010d8d6019ecc0ec069131f2c27
Jordan Kicklighter <jwkicklighter@yahoo.com>
2015-09-29 02:22:55 -0400

    Added initial chunk of custom fields.

--- includes/chapter.php
+++ includes/chapter.php
@@ -58,11 +58,63 @@ class SEDF_Chapter extends CPT_Core {
 
 		$box = new_cmb2_box( array(
 			'id'            => $prefix . 'metabox',
-			'title'         => __( 'SED Features Chapter Meta Box', 'sed-features' ),
+			'title'         => __( 'Chapter Information', 'sed-features' ),
 			'object_types'  => array( 'sedf-chapter', ),
 			'context'       => 'normal',
 			'priority'      => 'high',
 			'show_names'    => true, // Show field names on the left
+		) );
+
+		$box->add_field( array(
+	    'name'    => 'Greek Designation',
+	    'desc'    => 'should be spelled out (i.e. Alpha, not A)',
+	    'id'      => 'chapter_designation',
+	    'type'    => 'text_medium',
+		) );
+
+		$box->add_field( array(
+	    'name'    => 'Charter Date',
+	    'id'      => 'chapter_charter1',
+	    'type'    => 'text_date_timestamp',
+		) );
+
+		$box->add_field( array(
+	    'name'    => 'Recharter Date 1',
+	    'id'      => 'chapter_charter2',
+			'type'    => 'text_date_timestamp',
+		) );
+
+		$box->add_field( array(
+	    'name'    => 'Recharter Date 2',
+	    'id'      => 'chapter_charter3',
+			'type'    => 'text_date_timestamp',
+		) );
+
+		$box->add_field( array(
+	    'name'    => 'School Name',
+	    'id'      => 'chapter_school',
+	    'type'    => 'text_medium',
+		) );
+
+		$box->add_field( array(
+	    'name'    => 'Address',
+	    'id'      => 'chapter_address',
+	    'type'    => 'text_medium',
+		) );
+
+		$box->add_field( array(
+	    'name'    => 'Chapter Website',
+	    'id'      => 'chapter_url',
+	    'type'    => 'text_medium',
+		) );
+
+		$box->add_field( array(
+	    'name'             => 'Big Brother',
+	    'id'               => 'chapter_big_brother',
+	    'type'             => 'select',
+	    'show_option_none' => true,
+	    'default'          => 'custom',
+	    'options'          => 'all_chapters',
 		) );
 	}
 
